b44271f11fcf2b8a1c2adbbde8d57cf512770d9a
Roy Kakkenberg <me@roykakkenberg.nl>
2023-12-20 16:13:27 +0100

    Add filter for min/max number of dependencies/dependents

--- src/database/Neo4jClient.ts
+++ src/database/Neo4jClient.ts
@@ -13,6 +13,14 @@ export interface QueryOptions {
   onlyExternalRelations?: boolean,
   showDependencies?: boolean,
   showDependents?: boolean,
+  dependencyRange?: {
+    min?: number;
+    max?: number;
+  },
+  dependentRange?: {
+    min?: number;
+    max?: number;
+  },
 }
 
 type Neo4jComponentNode = Neo4jNode<Integer, {
@@ -76,7 +84,7 @@ export class Neo4jClient {
     if (chunks[0][0].type.toLowerCase() !== 'contains') chunks.unshift([]);
     // If we do not end with any CONTAIN edges, push an empty array to the back to indicate
     // that we do not have such edges
-    if (chunks[chunks.length - 1][0].type.toLowerCase() !== 'contains' || chunks.length === 1) chunks.push([]);
+    if (chunks[chunks.length - 1][0]?.type.toLowerCase() !== 'contains' || chunks.length === 1) chunks.push([]);
     return chunks;
   }
 
@@ -91,36 +99,15 @@ export class Neo4jClient {
     name: string,
     options: GraphFilterOptions = {},
   ): Graph {
-    console.log(records.length);
-    const { selectedId, maxDepth, reverseDirection } = options;
+    const {
+      selectedId, maxDepth, reverseDirection, minRelationships, maxRelationships,
+    } = options;
 
-    // Remove all paths that are too deep for the given
-    // parameters (because then we cannot create a transitive edge)
-    const filteredRecords = records.filter((record) => {
-      // No depth filter, so keep everything
-      if (maxDepth === undefined) return true;
-      const path = record.get('path');
-      if (path.length === 0) return true;
-
-      // Split the list of edge labels into chunks of the same labels
-      const chunks = this.groupRelationships(path);
-
-      const containsSourceDepth = chunks[0][0]?.type.toLowerCase() === 'contains' ? chunks[0].length : 0;
-      const containsTargetDepth = chunks[chunks.length - 1][0]?.type.toLowerCase() === 'contains' ? chunks[chunks.length - 1].length : 0;
-      if (!reverseDirection) {
-        const containsTooDeep = Math.max(0, containsSourceDepth - maxDepth);
-        // Target node layer is deeper than the maximum depth, so we should not keep this path
-        if (containsTargetDepth < containsTooDeep) return false;
-      } else {
-        const containsTooDeep = Math.max(0, containsTargetDepth - maxDepth);
-        // Source node layer is deeper than the maximum depth, so we should not keep this path
-        if (containsSourceDepth < containsTooDeep) return false;
-      }
-      return true;
-    });
-
+    // Process the nodes at the beginning, as records that are no maximal path are deleted
+    // Then, we lose crucial information on the nodes on these paths
+    // We should filter these nodes later; only keep the nodes that lay on one or more paths
     const seenNodes: string[] = [];
-    const nodes = filteredRecords
+    let nodes = records
       .map((r) => [r.get('source'), r.get('target')]
         .map((field): Node | undefined => {
           const nodeId = field.elementId;
@@ -144,10 +131,66 @@ export class Neo4jClient {
       .flat()
       .filter((node) => node !== undefined) as Node[];
 
-    const seenEdges: string[] = [];
-    const replaceMap = new Map<string, string>();
+    // Keep only the paths that go from selected node to the domain node of the relationship
+    // We have to delete any duplicates, because otherwise all these extra paths count towards
+    // the total number of relationship a leaf has.
+    const seenPaths = new Map<string, number>();
+    let filteredRecords = records.map((record, i, all) => record.toObject())
+      .map((record) => {
+        const { path } = record;
+        const chunks = this.groupRelationships(path);
+        const pathId = chunks.slice(1, chunks.length - 1).flat().map((e) => e.elementId).join(',');
+
+        let currDepth = 0;
+        if (seenPaths.has(pathId)) {
+          currDepth = seenPaths.get(pathId)!;
+        }
+
+        if (reverseDirection) {
+          seenPaths.set(pathId, Math.max(currDepth, chunks[0].length));
+        } else {
+          seenPaths.set(pathId, Math.max(currDepth, chunks[chunks.length - 1].length));
+        }
+        return record;
+      }).filter((record, i, all) => {
+        const { path } = record;
+        const chunks = this.groupRelationships(path);
+        const pathId = chunks.slice(1, chunks.length - 1).flat().map((e) => e.elementId).join(',');
+        const depth = seenPaths.get(pathId) || 0;
+
+        if (reverseDirection) {
+          return chunks[0].length === depth;
+        }
+        return chunks[chunks.length - 1].length === depth;
+      });
+
+    // Remove all paths that are too deep for the given
+    // parameters (because then we cannot create a transitive edge)
+    filteredRecords = filteredRecords.filter((record, i, all) => {
+      // No depth filter, so keep everything
+      if (maxDepth === undefined) return true;
+      const { path } = record;
+      if (path.length === 0) return true;
+
+      // Split the list of edge labels into chunks of the same labels
+      const chunks = this.groupRelationships(path);
+
+      const containsSourceDepth = chunks[0][0]?.type.toLowerCase() === 'contains' ? chunks[0].length : 0;
+      const containsTargetDepth = chunks[chunks.length - 1][0]?.type.toLowerCase() === 'contains' ? chunks[chunks.length - 1].length : 0;
+      if (!reverseDirection) {
+        const containsTooDeep = Math.max(0, containsSourceDepth - maxDepth);
+        // Target node layer is deeper than the maximum depth, so we should not keep this path
+        if (containsTargetDepth < containsTooDeep) return false;
+      } else {
+        const containsTooDeep = Math.max(0, containsTargetDepth - maxDepth);
+        // Source node layer is deeper than the maximum depth, so we should not keep this path
+        if (containsSourceDepth < containsTooDeep) return false;
+      }
+      return true;
+    });
 
     // Replace all transitive nodes with this existing end node (the first of the full path)
+    const replaceMap = new Map<string, string>();
     const addToReplaceMap = (deletedEdges: Neo4jComponentDependency[]) => {
       if (deletedEdges.length === 0) return;
       const firstStartNode = deletedEdges[0].startNodeElementId;
@@ -155,63 +198,145 @@ export class Neo4jClient {
         .set(edge.endNodeElementId, firstStartNode));
     };
 
-    const edges = filteredRecords
-      .map((record) => {
-        let path = record.get('path');
-        if (maxDepth !== undefined) {
-          const chunks = this.groupRelationships(path);
+    // Find the nodes that need to be replaced (and with which nodes).
+    // Also, already remove the too-deep nodes
+    if (maxDepth) {
+      filteredRecords = filteredRecords.map((record, i, all): Neo4jComponentGraph => {
+        const { path } = record;
+        const chunks = this.groupRelationships(path);
 
-          const containsSourceDepth = chunks[0][0]?.type.toLowerCase() === 'contains' ? chunks[0].length : 0;
-          const containsTargetDepth = chunks[chunks.length - 1][0]?.type.toLowerCase() === 'contains' ? chunks[chunks.length - 1].length : 0;
-          if (!reverseDirection) {
-            const containsTooDeep = Math.max(0, containsSourceDepth - maxDepth);
+        const containsSourceDepth = chunks[0][0]?.type.toLowerCase() === 'contains' ? chunks[0].length : 0;
+        const containsTargetDepth = chunks[chunks.length - 1][0]?.type.toLowerCase() === 'contains' ? chunks[chunks.length - 1].length : 0;
+        if (!reverseDirection) {
+          const containsTooDeep = Math.max(0, containsSourceDepth - maxDepth);
 
-            const deletedSource = chunks[0].splice(maxDepth, containsTooDeep);
-            addToReplaceMap(deletedSource);
+          const deletedSource = chunks[0].splice(maxDepth, containsTooDeep);
+          addToReplaceMap(deletedSource);
 
-            const deletedTarget = chunks[chunks.length - 1]
-              .splice(containsTargetDepth - containsTooDeep, containsTooDeep);
-            addToReplaceMap(deletedTarget);
-          } else {
-            const containsTooDeep = Math.max(0, containsTargetDepth - maxDepth);
+          const deletedTarget = chunks[chunks.length - 1]
+            .splice(containsTargetDepth - containsTooDeep, containsTooDeep);
+          addToReplaceMap(deletedTarget);
+        } else {
+          const containsTooDeep = Math.max(0, containsTargetDepth - maxDepth);
 
-            const deletedSource = chunks[0]
-              .splice(containsSourceDepth - containsTooDeep, containsTooDeep);
-            addToReplaceMap(deletedSource);
+          const deletedSource = chunks[0]
+            .splice(containsSourceDepth - containsTooDeep, containsTooDeep);
+          addToReplaceMap(deletedSource);
 
-            const deletedTarget = chunks[chunks.length - 1].splice(maxDepth, containsTooDeep);
-            addToReplaceMap(deletedTarget);
-          }
-
-          path = chunks.flat();
+          const deletedTarget = chunks[chunks.length - 1].splice(maxDepth, containsTooDeep);
+          addToReplaceMap(deletedTarget);
         }
-        return path.map((r): Edge | undefined => {
-          const edgeId = r.elementId;
-          if (seenEdges.indexOf(edgeId) >= 0) return undefined;
-          seenEdges.push(edgeId);
-          return {
-            data: {
-              id: r.elementId,
-              source: r.startNodeElementId,
-              target: r.endNodeElementId,
-              label: r.type.toLowerCase(),
-              properties: {
-                weight: 1,
-                traces: [],
-              },
+
+        return {
+          ...record,
+          path: [
+            ...chunks[0],
+            ...chunks.slice(1, chunks.length - 1).flat(),
+            ...chunks[chunks.length - 1],
+          ] as Neo4jComponentDependency[],
+        };
+        // Replace the source and target nodes of the dependency edges to make them transitive
+      }).map((record, i, all): Neo4jComponentGraph => {
+        const { path } = record;
+        const chunks = this.groupRelationships(path);
+        const toEdit = chunks.slice(1, chunks.length - 1);
+
+        return {
+          ...record,
+          path: [
+            ...chunks[0],
+            ...toEdit.map((chunk) => chunk.map((e): Neo4jComponentDependency => {
+              if (replaceMap.has(e.startNodeElementId)) {
+                e.startNodeElementId = replaceMap.get(e.startNodeElementId) as string;
+              }
+              if (replaceMap.has(e.endNodeElementId)) {
+                e.endNodeElementId = replaceMap.get(e.endNodeElementId) as string;
+              }
+              return e;
+            })).flat(),
+            ...chunks[chunks.length - 1],
+          ] as Neo4jComponentDependency[],
+        };
+      });
+    }
+
+    // Count how many relationships each child of the selected node has
+    const relationsMap = new Map<string, string[]>();
+    filteredRecords.forEach((record) => {
+      if (!minRelationships && !maxRelationships) return;
+
+      const chunks = this.groupRelationships(record.path);
+      if (chunks.length <= 1 || chunks[1].length === 0) return;
+
+      let node: string;
+      let relatedNode: string;
+      if (reverseDirection) {
+        const edge = chunks[chunks.length - 2][chunks[chunks.length - 2].length - 1];
+        node = edge?.endNodeElementId;
+        relatedNode = edge?.startNodeElementId;
+      } else {
+        // eslint-disable-next-line prefer-destructuring
+        const edge = chunks[1][0]; // Last element of first chunk
+        node = edge?.startNodeElementId;
+        relatedNode = edge?.endNodeElementId;
+      }
+
+      if (!relationsMap.get(node)?.includes(relatedNode)) {
+        const currentRelations = relationsMap.get(node) || [];
+        relationsMap.set(node, [...currentRelations, relatedNode]);
+      }
+    });
+
+    // Apply filter
+    filteredRecords = filteredRecords.filter((record) => {
+      const chunks = this.groupRelationships(record.path);
+      if (!minRelationships && !maxRelationships) return true;
+
+      let node: string;
+      if (reverseDirection) {
+        node = chunks[chunks.length - 2][chunks[chunks.length - 2].length - 1]?.endNodeElementId;
+      } else {
+        // eslint-disable-next-line prefer-destructuring
+        node = chunks[1][0]?.startNodeElementId; // Last element of first chunk
+      }
+
+      const uniqueRelationships = relationsMap.get(node) || [];
+      if (minRelationships && uniqueRelationships.length < minRelationships) return false;
+      if (maxRelationships && uniqueRelationships.length > maxRelationships) return false;
+      return true;
+    });
+
+    const seenEdges: string[] = [];
+    const edges = filteredRecords
+      .map((record) => record.path.map((r): Edge | undefined => {
+        const edgeId = r.elementId;
+        if (seenEdges.indexOf(edgeId) >= 0) return undefined;
+        seenEdges.push(edgeId);
+        return {
+          data: {
+            id: r.elementId,
+            source: r.startNodeElementId,
+            target: r.endNodeElementId,
+            label: r.type.toLowerCase(),
+            properties: {
+              weight: 1,
+              traces: [],
             },
-          };
-        });
-      })
+          },
+        };
+      }))
       .flat()
       .flat()
       .filter((edge) => edge !== undefined)
-      .map((edge): Edge => {
-        const e = edge!;
-        if (replaceMap.has(e.data.source)) e.data.source = replaceMap.get(e.data.source) as string;
-        if (replaceMap.has(e.data.target)) e.data.target = replaceMap.get(e.data.target) as string;
-        return e;
-      })
+      .map((edge): Edge => edge!)
+      // .map((edge): Edge => {
+      //   const e = edge!;
+      //   if (replaceMap.has(e.data.source)) e.data.source =
+    //   replaceMap.get(e.data.source) as string;
+      //   if (replaceMap.has(e.data.target)) e.data.target =
+    //   replaceMap.get(e.data.target) as string;
+      //   return e;
+      // })
       .reduce((newEdges: Edge[], edge) => {
         const index = newEdges.findIndex((e) => e.data.source === edge.data.source
             && e.data.target === edge.data.target);
@@ -220,6 +345,16 @@ export class Neo4jClient {
         newEdges[index].data.properties.weight += 1;
         return newEdges;
       }, []);
+
+    const nodesOnPaths: string[] = edges
+      // Get the source and target from each edge
+      .map((e) => [e.data.source, e.data.target])
+      // Flatten the 2D array
+      .flat()
+      // Remove duplicates
+      .filter((n1, i, all) => i === all.findIndex((n2) => n1 === n2));
+    // Filter the actual node objects
+    nodes = nodes.filter((n) => nodesOnPaths.includes(n.data.id));
 
     return {
       name,
@@ -313,7 +448,7 @@ export class Neo4jClient {
 
   async getDomainModules({
     id, layerDepth, dependencyDepth, onlyExternalRelations, onlyInternalRelations,
-    showDependencies, showDependents,
+    showDependencies, showDependents, dependencyRange, dependentRange,
   }: QueryOptions) {
     const buildQuery = (dependencies: boolean = true) => {
       let query = `
@@ -340,8 +475,23 @@ export class Neo4jClient {
       this.getChildren(id, layerDepth),
       this.getParents(id),
     ];
-    if (showDependencies) promises.push(this.executeAndProcessQuery(buildQuery(true), 'All dependencies and their parents', { selectedId: id, maxDepth: layerDepth }));
-    if (showDependents) promises.push(this.executeAndProcessQuery(buildQuery(false), 'All dependents and their parents', { selectedId: id, maxDepth: layerDepth, reverseDirection: true }));
+    if (showDependencies) {
+      promises.push(this.executeAndProcessQuery(buildQuery(true), 'All dependencies and their parents', {
+        selectedId: id,
+        maxDepth: layerDepth,
+        minRelationships: dependencyRange?.min,
+        maxRelationships: dependencyRange?.max,
+      }));
+    }
+    if (showDependents) {
+      promises.push(this.executeAndProcessQuery(buildQuery(false), 'All dependents and their parents', {
+        selectedId: id,
+        maxDepth: layerDepth,
+        reverseDirection: true,
+        minRelationships: dependentRange?.min,
+        maxRelationships: dependentRange?.max,
+      }));
+    }
 
     const graphs = await Promise.all(promises);
     return this.mergeGraphs(...graphs);
