3af4069461d73051408fec642d803a194d714229
Roy Kakkenberg <me@roykakkenberg.nl>
2024-01-17 11:46:51 +0100

    Add HTTP basic auth to all endpoints

--- src/http.ts
+++ src/http.ts
@@ -22,7 +22,32 @@ export default async function createHttp() {
     }),
   );
   app.use(bodyParser.json());
-  app.use(cors());
+  app.use(cors({ credentials: true, origin: process.env.HTTP_FRONTEND_URL }));
+
+  /**
+   * HTTP Basic Auth on all endpoints
+   */
+  app.use((req, res, next) => {
+    const auth = {
+      login: process.env.HTTP_BASIC_AUTH_USERNAME,
+      password: process.env.HTTP_BASIC_AUTH_PASSWORD,
+    }; // change this
+
+    // parse login and password from headers
+    const b64auth = (req.headers.authorization || '').split(' ')[1] || '';
+    const [login, password] = atob(b64auth).split(':');
+
+    // Verify login and password are set and correct
+    if (login && password && login === auth.login && password === auth.password) {
+      // Access granted...
+      return next();
+    }
+
+    // Access denied...
+    res.set('WWW-Authenticate', 'Basic realm="401"');
+    res.status(401).send('Authentication required.');
+    return res;
+  });
 
   RegisterRoutes(app);
 
