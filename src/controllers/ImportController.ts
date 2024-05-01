64f337a52dccbb2c636c5219d33a3e729601cb4e
Roy Kakkenberg <me@roykakkenberg.nl>
2024-05-01 10:29:53 +0200

    feat: parse input files in-memory and no longer writing to disk

--- src/controllers/ImportController.ts
+++ src/controllers/ImportController.ts
@@ -1,12 +1,11 @@
 import {
-  Controller, FormField, Post, Request, Res, Response, Route, Tags, UploadedFiles,
+  Controller, FormField, Post, Produces, Request, Res, Response, Route, Tags, UploadedFiles,
 } from 'tsoa';
 import express from 'express';
 import { TsoaResponse } from '@tsoa/runtime';
 import parseGraph from 'arvisan-input-parser/src/parser';
-import { writeEdgesToDisk, writeNodesToDisk } from 'arvisan-input-parser/src/csv';
+import { getCsvNodes, getCsvEdges } from 'arvisan-input-parser/src/csv';
 import multer from 'multer';
-import fs from 'fs';
 import archiver from 'archiver';
 import ErrorResponse from './responses/ErrorResponse';
 
@@ -15,6 +14,7 @@ import ErrorResponse from './responses/ErrorResponse';
 export class ImportController extends Controller {
   @Post('parse')
   @Response<ErrorResponse>(501, 'Graph importing disabled')
+  @Produces('application/zip')
   public async importGraph(
     @Request() request: express.Request,
       @Res() errorResponse: TsoaResponse<501, ErrorResponse>,
@@ -24,14 +24,20 @@ export class ImportController extends Controller {
       @UploadedFiles() integrationFiles?: Express.Multer.File[],
       @FormField() includeModuleLayerLayer?: string,
       @FormField() anonymize?: string,
-  ): Promise<any> {
+  ): Promise<void> {
     if (!process.env.GRAPH_IMPORT_LOCATION) {
       this.setStatus(501);
       errorResponse(501, { message: 'Graph importing is disabled. If you wish to use this feature, enable it in the environment variables.' });
-    } else if (!parseGraph || !writeNodesToDisk || !writeEdgesToDisk || !multer) {
+      return;
+    }
+    if (!parseGraph || !getCsvNodes || !getCsvEdges || !multer) {
       this.setStatus(501);
       errorResponse(501, { message: 'Graph importing is enabled, but some dependencies are missing. Make sure you have all optional dependencies installed.' });
+      return;
     }
+
+    const { res } = request;
+    if (!res) throw new Error('express.Response not found.');
 
     const graph = parseGraph(
       structureFiles?.map((f) => f.buffer),
@@ -42,38 +48,35 @@ export class ImportController extends Controller {
       anonymize === 'true',
     );
 
-    writeNodesToDisk(graph.elements.nodes);
-    writeEdgesToDisk(graph.elements.edges);
+    const nodesFileName = 'nodes.csv';
+    const edgesFileName = 'relationships.csv';
+    const outputFileName = 'output.zip';
 
-    const { res } = request;
+    const nodesBuffer = getCsvNodes(graph.elements.nodes);
+    const edgesBuffer = getCsvEdges(graph.elements.edges);
+
     await new Promise<void>((resolve, reject) => {
-      const output = fs.createWriteStream('output.zip');
       const archive = archiver('zip');
 
-      output.on('close', () => {
-        res?.sendFile('output.zip', { root: process.cwd() }, (err) => {
-          if (err) {
-            console.error('Could not send file', err);
-            reject(err);
-          } else {
-            resolve();
-          }
-        });
+      archive.on('close', () => {
+        res.send();
+        resolve();
       });
 
-      output.on('error', (e) => {
+      archive.on('error', (e) => {
         console.error(e);
         reject(e);
       });
 
-      archive.pipe(output);
+      res.setHeader('Content-Disposition', `attachment; filename="${outputFileName}"`);
+      res.setHeader('Content-Type', 'application/zip');
+      res.attachment(outputFileName);
+      archive.pipe(res);
 
-      archive.append('nodes.csv', { name: 'nodes.csv' });
-      archive.append('edges.csv', { name: 'edges.csv' });
+      archive.append(nodesBuffer, { name: nodesFileName });
+      archive.append(edgesBuffer, { name: edgesFileName });
 
       archive.finalize();
     });
-
-    return res;
   }
 }
